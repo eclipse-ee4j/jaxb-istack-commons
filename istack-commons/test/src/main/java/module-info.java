2942ad3f3c1840b43b66c03468301658647982c2
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-10-01 20:58:10 +0200

    update plugin versions, clean up poms, add missing module descriptor
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- istack-commons/test/src/main/java/module-info.java
+++ istack-commons/test/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -9,11 +9,11 @@
  */
 
 /**
- * istack-commons tool time utilities.
+ * Common test-related utility code for the istack components.
  */
-module com.sun.istack.tools {
+module com.sun.istack.test {
     requires java.logging;
     requires transitive java.xml;
 
-    exports com.sun.istack.tools;
+    exports com.sun.istack.test;
 }
