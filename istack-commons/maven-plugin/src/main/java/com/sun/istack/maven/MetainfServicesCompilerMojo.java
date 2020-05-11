edfa0e327ba5cbdd1f11e7359497ab3e93105efa
Lukas Jungmann <lukas.jungmann@oracle.com>
2020-05-11 21:40:13 +0200

    #60: marking thread safety in maven plugins
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- istack-commons/maven-plugin/src/main/java/com/sun/istack/maven/MetainfServicesCompilerMojo.java
+++ istack-commons/maven-plugin/src/main/java/com/sun/istack/maven/MetainfServicesCompilerMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -44,7 +44,7 @@ import java.util.zip.ZipFile;
  *
  * Phase: generate-sources
  */
-@Mojo(name = "metainf-services", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@Mojo(name = "metainf-services", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true)
 public class MetainfServicesCompilerMojo extends AbstractMojo {
 
     /**
